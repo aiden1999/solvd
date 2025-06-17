80780c5090793aaa254c72e1ec20d3e41303c021
Aiden Tempest <tempestaiden@gmail.com>
2025-06-17 16:14:30 +0100

    cross sudoku clauses

--- backend/sudoku_solving.py
+++ backend/sudoku_solving.py
@@ -29,6 +29,8 @@ def get_solution(
             match puzzle.subtype:
                 case "Butterfly Sudoku":
                     puzzle_clauses = make_butterfly_clauses(all_vars)
+                case "Cross Sudoku":
+                    puzzle_clauses = make_cross_clauses(all_vars)
                 case _:
                     pass
         case "variants":
@@ -67,7 +69,7 @@ def make_standard_clauses(
 
 
 def make_butterfly_clauses(all_vars: list[controller.data_structs.SudokuVar]) -> list[int]:
-    """Creates CNF clauses for a standard sudoku puzzle.
+    """Creates CNF clauses for a butterfly sudoku puzzle.
 
     Args:
         all_vars: list of all possible variables.
@@ -127,6 +129,85 @@ def make_butterfly_clauses(all_vars: list[controller.data_structs.SudokuVar]) ->
     )
     butterfly_clauses = cell_clauses + row_clauses + col_clauses + box_clauses
     return butterfly_clauses
+
+
+def make_cross_clauses(all_vars: list[controller.data_structs.SudokuVar]) -> list[int]:
+    """Create CNF clauses for a cross sudoku puzzle.
+
+    Args:
+        all_vars: list of all possible variables.
+
+    Returns:
+        list of CNF clauses.
+    """
+    cell_clauses = make_cell_clauses(all_vars, 21, 9)
+    top_puzzle = []
+    left_puzzle = []
+    center_puzzle = []
+    right_puzzle = []
+    bottom_puzzle = []
+    for var in all_vars:
+        match var.box:
+            case 0 | 1 | 2 | 3 | 4 | 5:
+                top_puzzle.append(var)
+            case 6 | 7 | 13 | 14 | 20 | 21:
+                left_puzzle.append(var)
+            case 8:
+                top_puzzle.append(var)
+                left_puzzle.append(var)
+                center_puzzle.append(var)
+            case 9:
+                top_puzzle.append(var)
+                center_puzzle.append(var)
+            case 10:
+                top_puzzle.append(var)
+                center_puzzle.append(var)
+                right_puzzle.append(var)
+            case 11 | 12 | 18 | 19 | 25 | 26:
+                right_puzzle.append(var)
+            case 15:
+                left_puzzle.append(var)
+                center_puzzle.append(var)
+            case 16:
+                center_puzzle.append(var)
+            case 17:
+                center_puzzle.append(var)
+            case 22:
+                left_puzzle.append(var)
+                center_puzzle.append(var)
+                bottom_puzzle.append(var)
+            case 23:
+                center_puzzle.append(var)
+                bottom_puzzle.append(var)
+            case 24:
+                center_puzzle.append(var)
+                right_puzzle.append(var)
+                bottom_puzzle.append(var)
+            case 27 | 28 | 29 | 30 | 31 | 32:
+                right_puzzle.append(var)
+    row_clauses = (
+        make_row_clauses(top_puzzle, 21, 9, 14)
+        + make_row_clauses(left_puzzle, 21, 9, 8)
+        + make_row_clauses(center_puzzle, 21, 9, 14)
+        + make_row_clauses(right_puzzle, 21, 9, 20)
+        + make_row_clauses(bottom_puzzle, 21, 9, 14)
+    )
+    column_clauses = (
+        make_column_clauses(top_puzzle, 21, 9, 8)
+        + make_column_clauses(left_puzzle, 21, 9, 14)
+        + make_column_clauses(center_puzzle, 21, 9, 14)
+        + make_column_clauses(right_puzzle, 21, 9, 14)
+        + make_column_clauses(bottom_puzzle, 21, 9, 20)
+    )
+    box_clauses = (
+        make_box_clauses(top_puzzle, 21, 9, 33)
+        + make_box_clauses(left_puzzle, 21, 9, 33)
+        + make_box_clauses(center_puzzle, 21, 9, 33)
+        + make_box_clauses(right_puzzle, 21, 9, 33)
+        + make_box_clauses(bottom_puzzle, 21, 9, 33)
+    )
+    cross_clauses = cell_clauses + row_clauses + column_clauses + box_clauses
+    return cross_clauses
 
 
 def make_known_value_clauses(
@@ -238,6 +319,7 @@ def make_box_clauses(
         vars: list of variables.
         dimension: size of sudoku.
         max_num: the highest number a cell can take.
+        total_boxes: number of boxes in the puzzle.
 
     Returns:
         list of CNF clauses.
